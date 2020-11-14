81f6680c167c154602df71a004f58e93143e71fa
SarunC4 <74345299+SarunC4@users.noreply.github.com>
2020-11-14 16:58:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,17 +20,17 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table class="table table-dark table-striped table-hover" width="600" border="1">
   <tr>
-    <th width="150"> <div align="center">Name </div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-    <th width="100"> <div align="center">Action </div></th>
+    <th width="25%"> <div align="center">Name </div></th>
+    <th width="35%"> <div align="center">Comment </div></th>
+    <th width="25%"> <div align="center">Link </div></th>
+    <th width="15%"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Name'];?> <div align="center"></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
     <td><?php echo $Result['Action'];?><a href="form.php" class="btn btn-info" role="button">Edit</a> <a href="#" class="btn btn-info" role="button">Link Button</a></td>
