7d095da4ea6555badf1235019a507737ad57c5ab
SarunC4 <74345299+SarunC4@users.noreply.github.com>
2020-11-20 22:14:01 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -8,6 +8,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script> <!-- Datatable -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script> <!-- Edit Icon -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
     <?php
@@ -24,7 +28,7 @@
 
     if (mysqli_query($conn, $sql)) {
         ?><div class="display-3 text-center mb-3">Deleted Successfully</div>
-        <div class="text-center"><a href="index.php" class="btn btn-warning">กลับ</a></div><?php
+    <?php
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
@@ -32,4 +36,5 @@
     mysqli_close($conn);
     ?>
   </body>
+  <div class="text-center"><a href="index.php" class="btn btn-warning"><i class="fa fa-home"></i></a></div>
 </html>
